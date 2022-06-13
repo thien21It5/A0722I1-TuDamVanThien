3ec4d5eb488ae1924dd9663308a154286acc140e
ThienDeNgu <thientdv.21it@vku.udn.vn>
2022-06-13 18:20:51 +0700

    new update

---
+++ ss5-HTML form and table/Bai_Tap/JS/baiTap2_Table.js
@@ -0,0 +1,13 @@
+var demo=document.getElementById("demo");
+demo.addEventListener("mouseover",mouseover);
+demo.addEventListener("mouseout",mouseout);
+demo.addEventListener("click",clickFunction);
+function mouseover(){
+    document.getElementById("demo").style.color="red";
+}
+function mouseout(){
+    document.getElementById("demo").style.color="blue"
+}
+function clickFunction(){
+    alert("Bài số 2 của phần Table");
+}
