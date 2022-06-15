2f1c089b5790994f243fb478f7607761070eb862
ThienDeNgu <thientdv.21it@vku.udn.vn>
2022-06-15 18:13:39 +0700

    update ss6

---
+++ ss6_JSoverview/thuc_hanh/js/thucHanh1.js
@@ -0,0 +1,23 @@
+// Hiển thị kết quả sử dụng hàm alert()
+
+function showMessage(){
+    alert("Xin chào");
+}
+
+// Hiển thị kết quả sử dụng thẻ HTML
+
+function showMessage1(){
+    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+}
+
+// Hiển thị kết quả sử dụng hàm document.write()
+
+function showMessage2(){
+    document.write("Xin chào!");
+}
+
+//Hiển thị kết quả sử dụng hàm console.log()
+
+function  showMessage3(){
+    console.log('Xin chào!');
+}
---
+++ ss6_JSoverview/thuc_hanh/js/thucHanh2.js
@@ -0,0 +1,11 @@
+//Bài 1
+
+function showMessage(){
+   document.write('Chào Mừng bạn tới CodeGym!');
+}
+
+//Bài 3
+function showMessage3(){
+   alert('Học viên CodeGym');
+   document.getElementById("showMess").innerHTML="Xin chào tôi học lớp A0522I1";
+}
